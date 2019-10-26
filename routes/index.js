54dcf96be38f330845622cf990d44b259f109fab
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-10-25 18:18:47 -0700

    Routing Links

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/school', function(req, res, next) {
+  res.render('school');
+});
+
 router.get('/logout', function(req,res,next){
   req.logout();
   req.flash('info', 'You have been logged out.');
