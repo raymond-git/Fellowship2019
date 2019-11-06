63a0265af72d2d27d5cddc374b84c21418a3cf80
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-11-05 18:50:35 -0800

    Added POST Request

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,38 @@ const router = express.Router();
 const models = require('../models');
 
 
-router.get('/', function(req, res, next) {
+router.get('/Home', function(req, res, next) {
   res.render('index');
 });
 
 router.get('/school', function(req, res, next) {
   console.log(req.query);
-  res.render('school', {name: req.query.name, 
-                        lastname: req.query.lastname});
+  res.render('school', {name: req.body.name, 
+                        lastname: req.body.lastname});
 });
+router.post('/school2', function(req, res, next) {
+  console.log(req.body);
+  res.render('school', {name: req.body.name, 
+                        lastname: req.body.lastname});
+
+});
+
+router.post('/school3', function(req, res, next) {
+  console.log(req.body);
+  res.render('school', {name: req.body.name, 
+                        lastname: req.body.lastname});
+  });
+
+  
+
+  
 
 router.get('/logout', function(req,res,next){
   req.logout();
   req.flash('info', 'You have been logged out.');
   res.redirect('/');
 });
+
+
 
 module.exports = router;
