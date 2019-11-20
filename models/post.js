2f0878ede003bce3a123599be3297a0129157216
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-11-19 18:45:29 -0800

    interacting with the database

--- models/post.js
+++ models/post.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    from: DataTypes.STRING,
-    title: DataTypes.STRING,
-    full_text: DataTypes.TEXT,
-    media_url: DataTypes.STRING
+  const Post = sequelize.define('Post', {  
+    from: DataTypes.STRING,   //column called from    Every column has a id
+    title: DataTypes.STRING,   //column called title
+    full_text: DataTypes.TEXT,   //column called full_text
+    media_url: DataTypes.STRING    //column called medial_url 
   }, {
     tableName: 'posts',
     underscored: true
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,14 @@ const models = require('../models');
 
 
 router.get('/Home', function(req, res, next) {
-  res.render('index');
-});
+  models.Post.findAll().then(function(records){  // findAll() is a function that finds all the data and send it back.
+                          // It will take sometime, maybe not now but we'll have all the data for sure
+                          // then(function(records) means once have the data fetch the data to me
+  res.render('index',{
+    records:records
+         });
+      });
+    });
 
 router.get('/school', function(req, res, next) {
   console.log(req.query);
