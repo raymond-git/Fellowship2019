896895ca4b0a054b3db73052a8ccd9b2a8bc6c3a
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-12-03 18:31:31 -0800

    Work on finding id

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,32 @@ const models = require('../models');
 
 
 router.get('/Home', function(req, res, next) {
-  models.Post.findAll().then(function(records){  // findAll() is a function that finds all the data and send it back.
+    models.Post.findAll().then(function(records){  // findAll() is a function that finds all the data and send it back.
                           // It will take sometime, maybe not now but we'll have all the data for sure
                           // then(function(records) means once have the data fetch the data to me
-  res.render('index',{
-    records:records
+    res.render('index',{
+            records:records
          });
       });
     });
 
 router.get('/school', function(req, res, next) {
-  console.log(req.query);
-  res.render('school', {name: req.body.name, 
+     console.log(req.query);
+     res.render('school', {name: req.body.name, 
                         lastname: req.body.lastname});
 });
 router.post('/school2', function(req, res, next) {
-  console.log(req.body);
-  res.render('school', {name: req.body.name, 
+     console.log(req.body);
+     res.render('school', {name: req.body.name, 
                         lastname: req.body.lastname});
 
 });
 
 router.post('/school3', function(req, res, next) {
-  console.log(req.body);
-  res.render('school', {name: req.body.name, 
+     console.log(req.body);
+     res.render('school', {name: req.body.name, 
                         lastname: req.body.lastname});
   });
-
-  
-
-  
 
 router.get('/logout', function(req,res,next){
   req.logout();
@@ -44,5 +40,11 @@ router.get('/logout', function(req,res,next){
 });
 
 
+router.get('/posts' , function(req, res, next){    
+  models.Post.findByPk(req.query.id).then(function(record){
+  res.render('post', {record:record})
+
+      });
+});
 
 module.exports = router;
