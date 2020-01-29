70537de0602fb71218cda7f9f502a672b06c9f77
raymond-git <52261775+raymond-git@users.noreply.github.com>
2020-01-28 18:57:13 -0800

    Edit form

--- routes/todos.js
+++ routes/todos.js
@@ -16,14 +16,45 @@ router.get('/', function(req, res, next) {
          res.render('todo/new');  
 
     });
+ 
 
     router.post('/' , function(req, res, next){
-        models.Todo.create({
+        //Creating a table to store all of the data
+        models.Todo.create({  
+        // Gather the information from html
             title: req.body.title,
             body: req.body.body
+        //Gather all the data 
         }).then(function(record){
+        //Post the information
             res.redirect(`/todo`);
         
         });
     });
+
+
+    
+    router.get('/:id', function(req, res, next){
+       models.Todo.findByPk(req.params.id).then(function(record){
+           res.render('todo/edit',{
+
+              record : record
+           });
+           
+       });
+    });
+
+    router.post('/:id', function(req, res, next){
+        models.Todo.findByPk(req.params.id).then(function(record){
+            record.update({
+                title: req.body.title,
+                body: req.body.body
+            }).then(function(record){
+                 res.redirect('/todo');
+
+                 })
+            })
+        });
+ 
+     
 module.exports = router;
